a909b67a800bd05addabea949040375fba27d03c
Nzoinova <nzoinova.contacto@gmail.com>
2026-02-27 12:02:25 +0100

    Fix base path format in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nors-apv/',
+  base: '/-nors-apv-/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
